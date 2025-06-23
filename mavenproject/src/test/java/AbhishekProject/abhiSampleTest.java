3ee8d2c1484001a84153ee652cee2e6dc829c465
lenovo <lenovo@DESKTOP-O9L4K3A>
2025-06-23 15:22:20 +0530

    First commit(Hello Guys)

---
+++ mavenproject/src/test/java/AbhishekProject/abhiSampleTest.java
@@ -0,0 +1,12 @@
+package AbhishekProject;
+
+import org.testng.annotations.Test;
+
+public class abhiSampleTest {
+	@Test
+	public void practiceGit() {
+		System.out.println("Hi Santosh How are You!!! Had Lunch???");
+		
+	}
+
+}
