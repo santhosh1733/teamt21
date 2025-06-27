2ab59950166427f94667ff33897a429393933902
Indresh Raja <indreshraja171@gmail.com>
2025-06-27 15:41:39 +0530

    mapla

--- newmavenproject/src/test/java/santhoshbranch/newclass.java
+++ newmavenproject/src/test/java/santhoshbranch/newclass.java
@@ -4,7 +4,11 @@ public class newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public void mapla() {
+		System.out.println("Maplaaa");
 	}
 
 }
