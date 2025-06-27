932fc4e5525f6107a55fbb358f629240575e563c
LENOVO <LENOVO@DESKTOP-8OEUUGM>
2025-06-27 15:58:44 +0530

    ADd xfbhs

--- newmavenproject/src/test/java/santhoshbranch/newclass.java
+++ newmavenproject/src/test/java/santhoshbranch/newclass.java
@@ -2,11 +2,9 @@ package santhoshbranch;
 
 public class newclass {
 
-	public static void main(String[] args) {}
-public void createdBygopika()
-{
-	System.out.println("hiii");
+	public static void main(String[] args) {
+		}
 	}
-}
+
 
 
